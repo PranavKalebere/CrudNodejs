855242f700432ba47b6f619df131c316f64e57a4
PranavKalebere <97215118+PranavKalebere@users.noreply.github.com>
2022-12-06 11:40:04 +0530

    Added curl in readme file

--- connection/mongodbConnection.js
+++ connection/mongodbConnection.js
@@ -9,25 +9,69 @@ mongoose.connect(constant.Mongodb_Connection,{useNewUrlParser:true,useUnifiedTop
 const db=mongoose.connection
 
 //Condition if DataBase connection is loss
-db.on('connecting', function() {
-    console.log(constant.Connecting);
-  });
+// db.on('connecting', function() {
+//     console.log(constant.Connecting);
+//   });
 
-  db.on('error', function(error) {
-    console.error(constant.Connection_Error + error);
-    mongoose.disconnect();
-  });
-  db.on('connected', function() {
-    console.log(constant.Db_Connect);
-  });
-  db.once('open', function() {
-    console.log(constant.Connection_Opened);
-  });
-  db.on('reconnected', function () {
-    console.log(constant.Db_Connection_Failed);
-  });
-  db.on('disconnected', function() {
-    console.log(constant.Connection_Disconnect);
+//   db.on('error', function(error) {
+//     console.error(constant.Connection_Error + error);
+//     mongoose.disconnect();
+//   });
+//   db.on('connected', function() {
+//     console.log(constant.Db_Connect);
+//   });
+//   db.once('open', function() {
+//     console.log(constant.Connection_Opened);
+//   });
+//   db.on('reconnected', function () {
+//     console.log(constant.Db_Connection_Failed);
+//   });
+//   db.on('disconnected', function() {
+//     console.log(constant.Connection_Disconnect);
+//     mongoose.connect(constant.Mongodb_Connection, {server:{auto_reconnect:true}});
+//   });
+//   mongoose.connect(constant.Mongodb_Connection, {server:{auto_reconnect:true}});
+
+
+
+  switch(db){
+  case 1:
+    db.on('connecting', function() {
+        console.log(constant.Connecting);
+      });
+      break;
+
+   case 2:
+    db.on('error', function(error) {
+        console.error(constant.Connection_Error + error);
+        mongoose.disconnect();
+      });
+      break;
+
+   case 3:
+    db.on('connected', function() {
+        console.log(constant.Db_Connect);
+      });
+       break;
+
+   case 4:
+    db.once('open', function() {
+        console.log(constant.Connection_Opened);
+      });
+      break;
+
+   case 5:
+    db.on('reconnected', function () {
+        console.log(constant.Db_Connection_Failed);
+      });
+      break;
+
+    case 6:
+        db.on('disconnected', function() {
+            console.log(constant.Connection_Disconnect);
+            mongoose.connect(constant.Mongodb_Connection, {server:{auto_reconnect:true}});
+          });    
+          break;
+    }
     mongoose.connect(constant.Mongodb_Connection, {server:{auto_reconnect:true}});
-  });
-  mongoose.connect(constant.Mongodb_Connection, {server:{auto_reconnect:true}});
+
